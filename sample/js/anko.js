f2bb31f8d174f1bdf158ba906c3f1630f990e80f
uchicom <2980920+uchicom@users.noreply.github.com>
2022-08-10 07:35:09 +0900

    #1

--- sample/js/anko.js
+++ sample/js/anko.js
@@ -1,4 +1,39 @@
 // (C) 2022 uchicom
+function idElement(id) {
+    return document.getElementById(id);
+}
+function removeAllChild(id) {
+	var select = idElement(id);
+	while (select.firstChild) {
+		select.removeChild(select.firstChild);
+	}
+	return select;
+}
+function replaceChild(id, child) {
+	removeAllChild(id).append(child);
+}
+function querySelector(selector) {
+	return document.querySelector(selector);
+}
+function querySelectorAll(selector) {
+	return document.querySelectorAll(selector);
+}
+function createClone(templateId, element)  {
+	const template = element ? element.getElementById(templateId) : document.getElementById(templateId);
+	return document.importNode(template.content, true);
+}
+function createRow(templateId, element) {
+	return createClone(templateId + ".row", element);
+}
+function createInput(templateId, element) {
+	return createClone(templateId + ".input", element);
+}
+function replaceTemplate(orgId, templateId, formFunction) {
+	const clone = createClone(templateId);
+	const form = clone.querySelector("form");
+	if (form && formFunction) formFunction(form);
+	document.getElementById(orgId).replaceWith(clone);
+}
 function postJson(url, assignHeader, payload, preProcess, process, errorProcess, postProcess) {
     if (preProcess) preProcess();
     fetch(url, {
