90f4ec633b1c93b5cd33d7b58490e3cfe724ec46
shigeki.uchiyama <shigeki.uchiyama@uchicom.com>
2025-09-20 21:05:49 +0900

    JettyからZouniに変更 #259

--- database/sql/create.sql
+++ database/sql/create.sql
@@ -4,11 +4,11 @@ create schema if not exists tracker;
 --account
 create table tracker.account(
 id bigint auto_increment,
+inserted varchar(256) not null,
+insert_datetime timestamp not null,
 updated varchar(256),
 update_datetime timestamp,
 update_seq integer,
-inserted varchar(256) not null,
-insert_datetime timestamp not null,
 login_id varchar(128) not null,
 password varbinary(128),
 name varchar(128) not null,
@@ -18,11 +18,11 @@ primary key(id)
 --issue
 create table tracker.issue(
 id bigint auto_increment,
+inserted varchar(256) not null,
+insert_datetime timestamp not null,
 updated varchar(256),
 update_datetime timestamp,
 update_seq integer,
-inserted varchar(256) not null,
-insert_datetime timestamp not null,
 account_id bigint not null,
 subject varchar(128) not null,
 detail text not null,
